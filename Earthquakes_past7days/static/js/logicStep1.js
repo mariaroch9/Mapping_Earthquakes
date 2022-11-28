2b9f0427cae15f50309658b3a869749f429535f1
Rochelle Rebello <mariaroch9@gmail.com>
2022-11-28 15:32:51 -0500

    Earthquakes past 7 days map

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,13 +18,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 10,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 })
 
@@ -33,7 +33,6 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/mariaroch9/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
@@ -42,15 +41,11 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
 
-    L.geoJson(data, {
-        myStyle,
-        onEachFeature: function(features, layer){
-          layer.bindPopup("<h2> Neighbourhood : " + features.properties.AREA_NAME + "</h2>")
-        }
-      }).addTo(map);
+    L.geoJson(data).addTo(map);
+    
     });
 
 
